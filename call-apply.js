910a7521f81824f5a133a50f445cbc032c0d8d90
Ahnaf Arif <2080arif@gmail.com>
2021-07-06 20:48:51 +0600

    all added

---
+++ call-apply.js
@@ -0,0 +1,8 @@
+const normalPerson = {
+    firstName : "Ahnaf",
+    lastName : "Arif",
+    getFullName : function () {
+        console.log(this.firstName, this.lastName)
+    }
+}
+console.log(normalPerson.lastName);
