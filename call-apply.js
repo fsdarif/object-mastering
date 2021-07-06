9dcc07fae90315abbe49d1461c6a0ad1e707ffe5
Ahnaf Arif <2080arif@gmail.com>
2021-07-06 22:25:26 +0600

    added

--- call-apply.js
+++ call-apply.js
@@ -8,20 +8,46 @@ const normalPerson = {
 console.log(normalPerson.lastName);
 
 
-const simplePerson = {
-    salary : 15000,
-    chargeBill : function (x) {
-        this.salary = this.salary - x;
-        return this.salary
-    } 
-}
+// const simplePerson = {
+//     salary : 15000,
+//     chargeBill : function (x) {
+//         this.salary = this.salary - x;
+//         return this.salary
+//     } 
+// }
 // simplePerson.chargeBill(5050);
 // console.log(simplePerson.salary);
 
 // usage of bind
+// const heroPerson = {
+//     salary : 30000,
+// }
+//bind
+
+// const heroBillCharge = simplePerson.chargeBill.bind(heroPerson);
+// heroBillCharge(1000);
+// console.log(heroPerson.salary);
+
+//call
+// simplePerson.chargeBill.call(heroPerson, 500);
+// simplePerson.chargeBill.call(heroPerson, 1500);
+// console.log(heroPerson.salary);
+
+// for multiple perameter
+
+const simplePerson = {
+    salary : 15000,
+    chargeBill : function (amount, tax, tips) {
+        this.salary = this.salary - amount - tax - tips ;
+        return this.salary
+    } 
+}
 const heroPerson = {
     salary : 30000,
 }
-const heroBillCharge = simplePerson.chargeBill.bind(heroPerson);
-heroBillCharge(1000);
+// simplePerson.chargeBill.call(heroPerson, 1000, 500, 500);
+// console.log(heroPerson.salary);
+
+simplePerson.chargeBill.apply(heroPerson,[1000, 2000, 3000 ]);
 console.log(heroPerson.salary);
+
