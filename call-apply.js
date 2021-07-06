0dbb77d466a0df6cbbca0eaa33478876016cae99
Ahnaf Arif <2080arif@gmail.com>
2021-07-06 21:04:07 +0600

    all added

--- call-apply.js
+++ call-apply.js
@@ -6,3 +6,14 @@ const normalPerson = {
     }
 }
 console.log(normalPerson.lastName);
+
+
+const simplePerson = {
+    salary : 15000,
+    chargeBill : function (x) {
+        this.salary = this.salary - x;
+        return this.salary
+    } 
+}
+simplePerson.chargeBill(5050);
+console.log(simplePerson.salary);
