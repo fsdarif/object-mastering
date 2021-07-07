7caa7011c7d7560123f2433c297ec56ade4060dd
Ahnaf Arif <2080arif@gmail.com>
2021-07-07 15:38:09 +0600

    added

--- rest-countries-api.js
+++ rest-countries-api.js
@@ -10,16 +10,28 @@ const displayCountries = countries => {
     for (let i = 0; i < countries.length; i++) {
         const country = countries[i];
         const countryDiv = document.createElement('div');
+        //css class to countryDiv
+        countryDiv.className = "country"
 
         //div elements
-        const h3 = document.createElement('h3');
-        h3.innerText = country.name;
-        const p = document.createElement('p');
-        p.innerText = country.capital;
+        // const h3 = document.createElement('h3');
+        // h3.innerText = country.name;
+        // const p = document.createElement('p');
+        // p.innerText = country.capital;
 
-        //div
-        countryDiv.appendChild(h3);
-        countryDiv.appendChild(p);
+        // //div
+        // countryDiv.appendChild(h3);
+        // countryDiv.appendChild(p);
+
+        //shortcut version
+
+
+        const countryInfo = ` 
+        <h3 class="country-name">${country.name} </h3>
+        <p>${country.capital} </p>
+
+        `
+        countryDiv.innerHTML = countryInfo;
         countriesDiv.appendChild(countryDiv)
 
 
